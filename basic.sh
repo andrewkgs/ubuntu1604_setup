6c3e07e540cc51717152c8924b57fecbdcce5a51
andrewkgs <andrewkuogs@gmail.com>
2018-09-20 22:49:24 +0800

    first commit

---
+++ basic.sh
@@ -0,0 +1,78 @@
+##########################################################
+# ****** This script is ONLY for Ubuntu 16.04 LTS ****** #
+##########################################################
+
+########## Basic Tools ##########
+
+sudo apt-get install vim
+##############################################
+# source: https://github.com/leomao/pika-vim #
+##############################################
+# cd
+# git clone --depth=1 https://github.com/andrewkgs/pika-vim.git
+# cd pika-vim/
+# ./deploy.py
+
+
+sudo apt-get install tmux
+############################################
+# source: https://github.com/gpakosz/.tmux #
+############################################
+# cd
+# git clone https://github.com/andrewkgs/.tmux.git
+# ln -s -f .tmux/.tmux.conf
+# cp .tmux/.tmux.conf.loacl .
+# sudo mv PowerlineSymbols.otf /usr/local/share/fonts
+# fc-cache -vf /usr/local/share/fonts/
+# sudo apt-get install fonts-powerline
+# source ~/.tmux.conf.local
+
+
+sudo apt-get install openssh-server
+# vim /etc/ssh/sshd_config
+# sudo service ssh restart
+
+
+sudo apt-get install git-core
+# git config --global user.name "AndrewKuo"
+# git config --global user.email "andrewkuogs@gmail.com"
+# git config --global.ui auto
+# git config --global color.branch auto
+# git config --global color.status auto
+
+
+############ CUDA Setup ############
+
+# Download specified CUDA driver package
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_9.2.148-1_amd64.deb
+sudo wget -O ${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+# Register driver package on your machine
+sudo dpkg -i ${CUDA_REPO_PKG}
+sudo apt-get update
+# install CUDA driver
+sudo apt-get install cuda-drivers
+sudo apt-get install cuda=9.2.148-1
+# Register & install CuDNN library
+sudo dpkg -i libcudnn7_7.2.1.38-1+cuda9.2_amd64.deb
+sudo apt install nvidia-cuda-toolkit
+sudo apt-get update
+sudo apt-get install -y python3-pip
+# upgrade pip
+sudo pip3 install --upgrade pip
+
+
+########## pip Module Installation ##########
+
+# deep learning framework
+sudo pip3 install tensorflow-gpu
+sudo pip3 install http://download.pytorch.org/whl/cu92/torch-0.4.1-cp35-cp35m-linux_x86_64.whl 
+sudo pip3 install torchvision
+sudo pip3 install tensorboardX
+sudo pip3 install keras
+
+# other pip toolkit
+sudo pip3 install opencv-contrib-python
+sudo pip3 install jupyter
+sudo pip3 install scikit-learn
+sudo pip3 install scikit-image
+
---
+++ optional.sh
@@ -0,0 +1,15 @@
+# Browser
+sudo apt-get install google-chrome-stable
+sudo apt-get remove --purge firefox
+sudo rm -rf .mozilla/firefox/
+sudo rm -rf /etc/firefox/
+sudo rm -rf /usr/lib/firefox-addons/
+
+# Traditional Chinese input
+sudo apt-get install hime
+
+# Server settings
+# install wine first...
+wget -O ~/Desktop/winbox.exe http://www.mikrotik.com/download.winbox.exe
+
+
