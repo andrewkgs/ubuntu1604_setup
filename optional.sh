405ccbc50acd05702f3475e26e7b77fda0ecdc11
Andrew Kuo <andrewkuogs@gmail.com>
2018-09-21 22:46:14 +0800

    add google chrome download commands

--- optional.sh
+++ optional.sh
@@ -4,6 +4,10 @@ sudo apt-get remove --purge firefox
 sudo rm -rf .mozilla/firefox/
 sudo rm -rf /etc/firefox/
 sudo rm -rf /usr/lib/firefox-addons/
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get -y install google-chrome-stable
 
 # Traditional Chinese input
 sudo apt-get install hime
@@ -11,5 +15,3 @@ sudo apt-get install hime
 # Server settings
 # install wine first...
 wget -O ~/Desktop/winbox.exe http://www.mikrotik.com/download/winbox.exe
-
-
