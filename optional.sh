37e9faf19de612a4921fe5ce8e7dcf00539b4027
Andrew Kuo <andrewkuogs@gmail.com>
2018-09-26 20:05:35 +0800

    Update optional.sh

--- optional.sh
+++ optional.sh
@@ -4,10 +4,10 @@ sudo apt-get remove --purge firefox
 sudo rm -rf .mozilla/firefox/
 sudo rm -rf /etc/firefox/
 sudo rm -rf /usr/lib/firefox-addons/
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update
-sudo apt-get -y install google-chrome-stable
+#wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
+#sudo apt-get update
+#sudo apt-get -y install google-chrome-stable
 
 # Traditional Chinese input
 sudo apt-get install hime
