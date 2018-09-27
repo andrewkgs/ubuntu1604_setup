955a0cc5495a640cc81c29c70b69210c0122c917
Andrew Kuo <andrewkuogs@gmail.com>
2018-09-27 20:58:03 +0800

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -10,6 +10,7 @@ sudo apt-get install git-core
 # git config --global.ui auto
 # git config --global color.branch auto
 # git config --global color.status auto
+# git config --global push.default simple
 
 
 sudo apt-get install vim
